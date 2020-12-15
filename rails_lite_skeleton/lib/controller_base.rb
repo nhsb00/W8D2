4c8cce0f52f0dd05ec1c8cf4721476d0946458c6
soobin <nhsb00@hotmail.com>
2020-12-15 17:09:00 -0500

    Phase II done

--- rails_lite_skeleton/lib/controller_base.rb
+++ rails_lite_skeleton/lib/controller_base.rb
@@ -8,33 +8,47 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Can't render twice!" if @already_built_response == true
+    @res.status = 302
+    @res["Location"] = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Can't render twice!" if @already_built_response == true
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    render_content(ERB.new(File.read(File.join( File.dirname(__File__), "..", "views", self.calls.name.underscore, "#{template_name}.html.erb"))).result(binding),"text/html")
   end
 
   # method exposing a `Session` object
   def session
+    
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    
   end
 end
 
